3303cf0f326522582ccf29250f9ff09c02756d8a
Yuri Pogodaev <pogodaev.yuri@gmail.com>
2020-06-21 14:38:20 +0300

    Patch 3.15.1 InMemoryMealRepository and InMemoryUserRepository

---
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -0,0 +1,39 @@
+package ru.javawebinar.topjava.repository.inmemory;
+
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static ru.javawebinar.topjava.UserTestData.ADMIN;
+import static ru.javawebinar.topjava.UserTestData.USER;
+
+
+@Repository
+public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
+
+    public void init() {
+        map.clear();
+        map.put(UserTestData.USER_ID, USER);
+        map.put(UserTestData.ADMIN_ID, ADMIN);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return getCollection().stream()
+                .sorted(Comparator.comparing(User::getName).thenComparing(User::getEmail))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return getCollection().stream()
+                .filter(u -> email.equals(u.getEmail()))
+                .findFirst()
+                .orElse(null);
+    }
+}
